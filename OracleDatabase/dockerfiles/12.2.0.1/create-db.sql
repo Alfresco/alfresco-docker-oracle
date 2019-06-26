d9f873af6c0a3ce7527ee757bf108807bf31dc4a
Alex Mukha <alex.mukha@alfresco.com>
2019-06-26 09:33:05 +0100

    Revert "REPO-2630 Change script to build a DB image with Alfresco DB preinstalled"
    This reverts commit 8e06fa62ea1e3fa1bf4c714ea55d929ad0261c2c.

--- OracleDatabase/dockerfiles/12.2.0.1/create-db.sql
+++
@@ -1,7 +0,0 @@
-startup
---alter pluggable database pdb1 open;
---alter session set container = pdb1;
-create user alfresco identified by alfresco;
--- container = current;
-grant connect, resource, unlimited tablespace to alfresco;
-exit
